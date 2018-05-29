81a096542c2129d4455fdfb62a916a9106d9e480
Mike Kvederis <m_kvederis@msn.com>
2018-05-29 11:04:46 -0700

    got pic working

--- src/components/WeatherListItem.js
+++ src/components/WeatherListItem.js
@@ -5,14 +5,15 @@ class WeatherListItem extends Component {
     render() {
         const { day } = this.props;
         const date = new Date(day.dt * 1000);
+        const img = `http://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+
         return (
             <div className="weather-list-item">
-                <h2>{date.getMonth() + 1} / {date.getDate()}</h2>
+                <h2> <img src={img} alt={day.weather[0].description} /> {date.getMonth() + 1} / {date.getDate()}</h2>
                 <h3>{day.temp.min.toFixed(1)}&deg;F &#124; {day.temp.max.toFixed(1)}&deg;F</h3>
-            </div>
+            </div >
         );
     }
-
 }
 
 export default WeatherListItem;
